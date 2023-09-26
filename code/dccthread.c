12c1991013af9f2fffd7677e4e16f5707d60632a
tarcizio.lafaiete <tarcizio.lafaiete@inventvision.com.br>
2023-09-26 13:01:47 -0300

    Todos os testes da parte 1 foram resolvidos. Iniciei codigo da parte 2

--- code/dccthread.c
+++ code/dccthread.c
@@ -2,37 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dccthread.h"
+#include "dlist.h"
 
 
 typedef struct dccthread{
-    ucontext_t context;
     const char* name;
+    ucontext_t context;
+    int expected;
+    int execution;
 } dccthread_t;
 
 typedef struct {
     ucontext_t manager;
-    dccthread_t main;
-    dccthread_t* secundary[100];
-    int num_of_threads;
-    int current_thread;
+    struct dlist* ready_queue;
+    dccthread_t* current_thread;
+    dccthread_t* waiter;
 } managerThreads;
 
 managerThreads central;
 
 void managerCentral(){
-    
-    getcontext(&central.manager);
-    swapcontext(&central.manager,&central.main.context);
 
-    int i = 0;
-    getcontext(&central.manager);
-    printf("AAA\n");
-    while(central.secundary[central.current_thread] != NULL){
-        if(central.current_thread + 1 < central.num_of_threads){
-            central.current_thread++;
-        }
-        swapcontext(&central.manager,&central.secundary[central.current_thread - 1]->context);
-    }
+    dccthread_t* thread = dlist_pop_left(central.ready_queue);
+    central.current_thread = thread;
+    setcontext(&thread->context);
 }
 
 void dccthread_init(void (*func)(int),int param){
@@ -48,53 +41,85 @@ void dccthread_init(void (*func)(int),int param){
     makecontext(&central.manager,managerCentral,0);
     
      //Create a main Thread
-    getcontext(&central.main.context);
-    central.main.context.uc_stack.ss_size = THREAD_STACK_SIZE;
-    central.main.context.uc_stack.ss_flags = 0;
-    if((central.main.context.uc_stack.ss_sp = malloc(central.main.context.uc_stack.ss_size)) == NULL){
+     dccthread_t* main = (dccthread_t*)malloc(sizeof(dccthread_t));
+    getcontext(&(main->context));
+    main->context.uc_stack.ss_size = THREAD_STACK_SIZE;
+    main->context.uc_stack.ss_flags = 0;
+    if((main->context.uc_stack.ss_sp = malloc(main->context.uc_stack.ss_size)) == NULL){
         perror("Failing allocating space to main thread");
         exit(1);
     }
-    central.main.name = "main";
-    central.main.context.uc_link = &central.manager;
-    makecontext(&central.main.context,func,1,param);
+    main->name = "main";
+    main->execution = 0;
+    main->expected = 0;
+    main->context.uc_link = &central.manager;
+    makecontext(&(main->context),(void(*)(void))func,1,param);
 
-    for(int i = 0;i < 100;i++){
-        central.secundary[i] = (dccthread_t*) NULL;
-    }
-   
-   central.num_of_threads = 0;
-   central.current_thread = 0;
+    central.ready_queue = dlist_create();
+    dlist_push_right(central.ready_queue,main);
+
+    // central.waiterSignal = 0;
+
 
     setcontext(&central.manager);
+
     exit(EXIT_SUCCESS);
 
 }
 
 dccthread_t* dccthread_create(const char* name, void (*func)(int),int param){
-    dccthread_t newThread;
-    newThread.name = name;
+    dccthread_t* newThread = (dccthread_t*)malloc(sizeof(dccthread_t));
+    newThread->name = name;
 
-    getcontext(&newThread.context);
-    newThread.context.uc_stack.ss_size = THREAD_STACK_SIZE;
-    newThread.context.uc_stack.ss_flags = 0;
-    if((newThread.context.uc_stack.ss_sp = malloc(newThread.context.uc_stack.ss_size)) == NULL){
+    getcontext(&(newThread->context));
+    newThread->context.uc_stack.ss_size = THREAD_STACK_SIZE;
+    newThread->context.uc_stack.ss_flags = 0;
+    if((newThread->context.uc_stack.ss_sp = malloc(newThread->context.uc_stack.ss_size)) == NULL){
         perror("Failing allocationg space to new thread");
         return NULL;
     }
-    newThread.context.uc_link = &central.manager;
-    makecontext(&newThread.context,func,1,param);
+    newThread->expected = 0;
+    newThread->execution = 0;
+    newThread->context.uc_link = &central.manager;
+    makecontext(&(newThread->context),(void(*)(void))func,1,param);
+    
+    dlist_push_right(central.ready_queue,newThread);
 
-    central.num_of_threads++;
+    // setcontext(&central.manager);
 
-    int i = -1;
-    do{
-        i++;
-        if(central.secundary[i] == NULL){
-            central.secundary[i] = &newThread;
-        }
-    } while(central.secundary[i] == NULL);
+    return newThread;
 
-    return &newThread;
+}
+
+void dccthread_yield(){
+    
+    // dlist_push_right(central.ready_queue,central.current_thread);
+    getcontext(&central.current_thread->context);
+    dlist_push_right(central.ready_queue,central.current_thread);
+    swapcontext(&central.current_thread->context,&central.manager);
+    
+
+}
+
+dccthread_t * dccthread_self(){
+    return central.current_thread;
+}
+
+const char * dccthread_name(dccthread_t *tid){
+    return tid->name;
+}
+
+void dccthread_wait(dccthread_t* tid){
+    
+}
+
+void dccthread_exit(){
+
+    if(!central.current_thread->expected){
+        swapcontext(&central.current_thread->context,&central.manager);
+    }
+    else{
+        setcontext(&central.waiter->context);
+    }
 
 }
