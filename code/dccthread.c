ab7f0078e431415c272f9345a00eaffb67375cc5
tarcizio.lafaiete <tarcizio.lafaiete@inventvision.com.br>
2023-10-06 15:23:35 -0300

    Codigo Passou em todos os testes. Possivelmente Versao final

--- code/dccthread.c
+++ code/dccthread.c
@@ -13,6 +13,7 @@ typedef struct dccthread{
     int id;
     int blocking;
     int idWaited;
+    dccthread_t* waiting;
 } dccthread_t;
 
 typedef struct{
@@ -25,15 +26,19 @@ typedef struct{
 typedef struct {
     ucontext_t manager;
     struct dlist* ready_queue;
+    struct dlist* wait_queue;
+    struct dlist* sleep_queue;
     dccthread_t* current_thread;
     dccthread_t* sleep_thread;
     int globalID;
+    int wait;
     timerControl timer;
     timerControl sleeper;
+    sigset_t mask;
 } managerThreads;
 
-// #define block() sigprocmask( SIG_BLOCK, &central.block_timer, NULL)
-// #define unblock() sigprocmask( SIG_UNBLOCK, &central.block_timer, NULL)
+// #define block() sigprocmask( SIG_BLOCK, &central.mask, NULL)
+// #define unblock() sigprocmask( SIG_UNBLOCK, &central.mask, NULL)
 // #define block() {central.control.signal = 0;}
 // #define unblock() {central.control.signal = 1;}
 #define SIGNAL_BLOCK SIGUSR1
@@ -53,10 +58,11 @@ void __expired(int signo){
 void __sleep(){
         // if(signo == SIGNAL_SLEEP){
             getcontext(&central.current_thread->context);
-            dlist_push_right(central.ready_queue,central.sleep_thread);
+            dccthread_t* sleep_thread = dlist_pop_left(central.sleep_queue);
+            dlist_push_right(central.ready_queue,sleep_thread);
             timer_delete(central.sleeper.timer_id);
-            // swapcontext(&central.current_thread->context,&central.manager);
-            setcontext(&central.current_thread->context);
+            swapcontext(&central.current_thread->context,&central.manager);
+            // setcontext(&central.current_thread->context);
         // }
 }
 
@@ -127,19 +133,42 @@ void unblock(){
     create_timer(10);
 }
 
+int dlist_find(struct dlist* queue, dccthread_t* thread){
+    if(dlist_empty(queue)){
+        return 0;
+    }
+    struct dnode *curr = queue->head;
+    while(curr != NULL && (dccthread_t*)curr->data != thread){
+        curr = curr->next;
+    }
+    if(curr != NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
 void managerCentral(){
 
-    // block();
     dccthread_t* thread = dlist_pop_left(central.ready_queue);
     central.current_thread = thread;
     setcontext(&thread->context);
-    // unblock();
+
 }
 
 void dccthread_init(void (*func)(int),int param){
-    
+
     //Create a Manager Thread
     getcontext(&central.manager);
+
+    // sigemptyset(&central.mask);
+    // sigaddset(&central.mask,SIGNAL_BLOCK);
+    // sigaddset(&central.mask,SIGNAL_SLEEP);
+    // sigprocmask(SIG_SETMASK,&central.mask,NULL);
+
+    // central.manager.uc_sigmask = central.mask;
+
     central.manager.uc_stack.ss_size = THREAD_STACK_SIZE;
     central.manager.uc_stack.ss_flags = 0;
     if((central.manager.uc_stack.ss_sp = malloc(central.manager.uc_stack.ss_size)) == NULL){
@@ -149,27 +178,33 @@ void dccthread_init(void (*func)(int),int param){
     makecontext(&central.manager,managerCentral,0);
     
     //Create a main Thread
-    dccthread_t* main = (dccthread_t*)malloc(sizeof(dccthread_t));
-    getcontext(&(main->context));
-    main->context.uc_stack.ss_size = THREAD_STACK_SIZE;
-    main->context.uc_stack.ss_flags = 0;
-    if((main->context.uc_stack.ss_sp = malloc(main->context.uc_stack.ss_size)) == NULL){
+    dccthread_t* firstThread = (dccthread_t*)malloc(sizeof(dccthread_t));
+    getcontext(&(firstThread->context));
+    firstThread->context.uc_stack.ss_size = THREAD_STACK_SIZE;
+    firstThread->context.uc_stack.ss_flags = 0;
+    if((firstThread->context.uc_stack.ss_sp = malloc(firstThread->context.uc_stack.ss_size)) == NULL){
         perror("Failing allocating space to main thread");
         exit(1);
     }
     // main->name = "main";
-    strcpy(main->name,"main");
-    main->id = 0;
-    main->context.uc_link = &central.manager;
-    makecontext(&(main->context),(void(*)(void))func,1,param);
+    strcpy(firstThread->name,"main");
+    firstThread->id = 0;
+    firstThread->idWaited = -1;
+    firstThread->context.uc_link = &central.manager;
+    makecontext(&(firstThread->context),(void(*)(void))func,1,param);
 
     central.ready_queue = dlist_create();
-    dlist_push_right(central.ready_queue,main);
+    central.wait_queue = dlist_create();
+    central.sleep_queue = dlist_create();
+    dlist_push_right(central.ready_queue,firstThread);
 
     central.globalID = 0;
+    central.wait = 0;
+    // central.sleep_thread->id = 0;
 
     create_timer(10);
 
+    // managerCentral();
     setcontext(&central.manager);
 
     exit(EXIT_SUCCESS);
@@ -183,6 +218,7 @@ dccthread_t* dccthread_create(const char* name, void (*func)(int),int param){
     strcpy(newThread->name,name);
 
     newThread->id = central.globalID++;
+    newThread->idWaited = -1;
 
     getcontext(&(newThread->context));
     newThread->context.uc_stack.ss_size = THREAD_STACK_SIZE;
@@ -220,25 +256,30 @@ const char * dccthread_name(dccthread_t *tid){
 void dccthread_wait(dccthread_t* tid){
     
     block();
-    if(tid->id >= central.current_thread->id){
+    // printf("thread %s \n",tid->name);
+    // printf("sleep %s \n",central.sleep_thread->name);
+    if(dlist_find(central.ready_queue,tid) || dlist_find(central.sleep_queue,tid)){
         getcontext(&central.current_thread->context);
-        struct dlist* tmp = dlist_create();
-        dlist_push_right(tmp,tid);
-        dlist_push_right(tmp,central.current_thread);
-        dccthread_t* thread;
-        while(!dlist_empty(central.ready_queue)){
-            thread = dlist_pop_left(central.ready_queue);
-            dlist_push_right(tmp,thread);
-        }
-        central.ready_queue = tmp;
+        tid->idWaited = central.current_thread->id;
+        // printf("%d waited \n",tid->idWaited);
+        dlist_push_right(central.wait_queue,central.current_thread);
+        dlist_push_right(central.ready_queue,tid);
+        // printf("size %d \n",central.ready_queue->count);
         swapcontext(&central.current_thread->context,&central.manager);
     }
+
     unblock();
 }
 
 void dccthread_exit(){
     block();
+    // printf("current: %s ready size %d \n", central.current_thread->name,central.ready_queue->count);
+    if(central.current_thread->idWaited != -1){
+        dccthread_t* thread = dlist_pop_left(central.wait_queue);
+        dlist_push_right(central.ready_queue,thread);
+    }
     swapcontext(&central.current_thread->context,&central.manager);
+    // setcontext(&central.manager);
     unblock();
 }
 
@@ -247,9 +288,10 @@ void dccthread_sleep(struct timespec ts){
     block();
  
     getcontext(&central.current_thread->context);
-    central.sleep_thread = central.current_thread;
+    dlist_push_right(central.sleep_queue,central.current_thread);
     create_sleep(ts);
     
+    // setcontext(&central.manager);
     swapcontext(&central.current_thread->context,&central.manager);
 
     unblock();
